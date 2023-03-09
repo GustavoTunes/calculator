2f55239a495c6b16e965ab20b911e5bbe385a324
GustavoTunes <gustavosouzatunes@gmail.com>
2023-03-09 09:44:59 -0300

    Update calculating method from the left to right

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,27 @@ const calculator = {
     result: 0,
 
     add_operation(operation) {  
-      this.operations.push(operation);
+        this.operations.push(operation);
     },
 
     remove_operation() {
         this.operations.pop();
-      },
+    },
 
     clear_operations() {
         this.operations = [];
     },
 
+    calculate_operations_reducer(expression, operation) {
+        if (expression.length == 2) {
+            return [eval(expression.concat([operation]).join(" "))];
+        } else {
+            return expression.concat([operation]);
+        }
+    },
+        
     calculate_operations() {
-        return eval(this.operations.join(" "));
+        return this.operations.reduce(this.calculate_operations_reducer, []).pop();
     },
 
     show_result() {
--- test/main.test.js
+++ test/main.test.js
@@ -32,3 +32,25 @@ test('It sum 5 with 2 and after multiple by 2', () => {
     // Verify
     expect(calculator.calculate_operations()).toStrictEqual(14);
 });
+
+test("It's able to calculate numbers with more than two decimal places", () => {
+    // Setup 
+    calculator.clear_operations();
+    // Exercise
+    calculator.add_operation('20');
+    calculator.add_operation('+');
+    calculator.add_operation('100');
+    // Verify
+    expect(calculator.calculate_operations()).toStrictEqual(120);
+});
+
+test("It's able to calculte decimal numbers", () => {
+    // Setup 
+    calculator.clear_operations();
+    // Exercise
+    calculator.add_operation('1.5');
+    calculator.add_operation('+');
+    calculator.add_operation('2');
+    // Verify
+    expect(calculator.calculate_operations()).toStrictEqual(3.5);
+});
